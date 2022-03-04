95c98ddbb0c60d58c3ab2478f2ebb09e79666369
trantien <tvtien139@gmail.com>
2022-03-04 17:34:46 +0700

    update login

--- src/partials/layout.tsx
+++ src/partials/layout.tsx
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
+import React, {useState, FC, useEffect} from 'react';
 // @ts-ignore
 import { connect } from 'react-redux'
 import { getLogin, getLogout } from '../redux/actions'
 // @ts-ignore
 import { NavLink } from 'react-router-dom'
+import { Button, Modal, Form, Input, Checkbox } from 'antd'
 
 // @ts-ignore
-export const Layout = ({children}) => {
+const Layout: FC<any> = ({children, ...props}) => {
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	const showModal = () => {
+		setIsModalVisible(true);
+	};
+
+	const handleOk = () => {
+		setIsModalVisible(false);
+	};
+
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+
+	const onFinish = async (values: any) => {
+		console.log('Success:', values);
+		props.onLogin(values.username, values.password)
+	};
+
+	const onFinishFailed = (errorInfo: any) => {
+		console.log('Failed:', errorInfo);
+	};
+
+	useEffect(()=>{
+		if(props.isLogin === true) setIsModalVisible(false);
+		console.log(props.isLogin, "props.isLogin")
+	}, [props.isLogin])
+
+	// @ts-ignore
 	return (
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,7 +49,47 @@ export const Layout = ({children}) => {
 					<div className="navbar-nav">
 						<NavLink to={'/demo/1'} className="nav-item nav-link">Trang demo 1</NavLink>
 						<NavLink to={'/demo'} className="nav-item nav-link">Trang demo 2</NavLink>
+						{!props.isLogin && <a><Button onClick={showModal} type="primary">Login</Button></a>}
+						{props.isLogin && <a><Button onClick={props.onLogout} type="primary">{props.data && props.data.name}</Button></a>}
 					</div>
+					<Modal title="Login" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
+						<Form
+							name="basic"
+							labelCol={{ span: 4 }}
+							wrapperCol={{ span: 20 }}
+							initialValues={{ remember: true }}
+							onFinish={onFinish}
+							onFinishFailed={onFinishFailed}
+							autoComplete="off"
+						>
+							<Form.Item
+								label="Username"
+								name="username"
+								rules={[{ required: true, message: 'Please input your username!' }]}
+							>
+								<Input />
+							</Form.Item>
+
+							<Form.Item
+								label="Password"
+								name="password"
+								rules={[{ required: true, message: 'Please input your password!' }]}
+							>
+								<Input.Password />
+							</Form.Item>
+
+							<Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 4, span: 20 }}>
+								<Checkbox>Remember me</Checkbox>
+							</Form.Item>
+
+							<Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+								<Button type="primary" htmlType="submit">
+									Submit
+								</Button>
+							</Form.Item>
+						</Form>
+
+					</Modal>
 				</div>
 				</div>
 			</nav>
